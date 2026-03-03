e2126c393a5d86471f283d539ad853abbb482f91
Your Name <you@example.com>
2026-03-03 12:39:14 +0530

    added footer into it

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+
+
+function Footer(){
+
+
+    return(
+        <footer style={{width:"100%",height:"300px",backgroundColor:"black"}}>
+
+             <h1>This is the footer component </h1>
+
+        </footer>
+    )
+}
+
+export default Footer;
